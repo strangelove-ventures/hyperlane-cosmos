8473d9711131bd9a38021f219623a0d635dc48c1
Kyle <kyle@strange.love>
2023-08-17 12:59:30 -0400

    remove dead code

--- x/igp/keeper/store.go
+++ x/igp/keeper/store.go
@@ -19,12 +19,6 @@ func (k Keeper) getGasPaidStore(ctx sdk.Context, destination uint32, relayer sdk
 	return prefix.NewStore(store, append(domainKey, relayer...))
 }
 
-// getDefaultRelayer gets the global default relayer. Note: at present, relayers are not configured per-destination.
-func (k Keeper) getDefaultRelayer(ctx sdk.Context) sdk.AccAddress {
-	store := ctx.KVStore(k.storeKey)
-	return store.Get(types.DefaultRelayerKey)
-}
-
 func (k Keeper) getIgpStore(ctx sdk.Context) prefix.Store {
 	store := ctx.KVStore(k.storeKey)
 	return prefix.NewStore(store, types.IgpKey)
@@ -59,27 +53,5 @@ func (k Keeper) setIgp(ctx sdk.Context, igp *types.Igp) error {
 		return err
 	}
 	store.Set(igpIdB, igpB)
-	return nil
-}
-
-// createIgp creates the IGP with the given owner and stores it
-func (k Keeper) createIgp(ctx sdk.Context, igp_id uint32, owner sdk.AccAddress) error {
-	store := k.getIgpStore(ctx)
-	igpIdB := make([]byte, 4)
-	binary.LittleEndian.PutUint32(igpIdB, igp_id)
-	if store.Has(igpIdB) {
-		return types.ErrInvalidIgp.Wrapf("igp %d already exists and cannot be created", igp_id)
-	}
-
-	newIgp := types.Igp{
-		Owner: owner.String(),
-		IgpId: igp_id,
-	}
-
-	igp, err := newIgp.Marshal()
-	if err != nil {
-		return err
-	}
-	store.Set(igpIdB, igp)
 	return nil
 }
